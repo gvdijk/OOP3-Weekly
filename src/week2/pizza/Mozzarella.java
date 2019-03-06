a76a4c0f947d22f118d4c9f8df5856f72a05b985
JustinZuidgeest <justinzuidgeest@gmail.com>
2019-03-06 16:31:58 +0100

    Pizza fix

--- src/week2/pizza/Mozzarella.java
+++ src/week2/pizza/Mozzarella.java
@@ -25,14 +25,4 @@ public class Mozzarella extends ToppingDecorator {
         System.out.println("Cost of mozzarella: " + cost);
         return tempPizza.getCost() + cost;
     }
-
-    @Override
-    public String getSize() {
-        return tempPizza.getSize();
-    }
-
-    @Override
-    public void setSize(String size) {
-        tempPizza.setSize(size);
-    }
 }
