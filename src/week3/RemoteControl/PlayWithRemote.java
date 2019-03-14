afe7876a268b25ed3a91ee0d4402b06bf378f5ef
JustinZuidgeest <justinzuidgeest@gmail.com>
2019-03-14 13:57:49 +0100

    Update #2

--- src/week3/RemoteControl/PlayWithRemote.java
+++ src/week3/RemoteControl/PlayWithRemote.java
@@ -12,13 +12,13 @@ public class PlayWithRemote{
         // create command receiver for stereo
         ElectronicDevice stereoDevice = new StereoReceiver();
         
-        // create all concrete commands for the tv, register the receiver
-        TurnTVOn tvOnCommand = new TurnTVOn(tvDevice);
+        // create all buttons for the tv
+        DeviceButton tvOnButton = new DeviceButton(new TurnTVOn(tvDevice), "TV ON");
         TurnTVOff tvOffCommand = new TurnTVOff(tvDevice);
         TvVolumeUp tvVolumeUpCommand = new TvVolumeUp(tvDevice);
         TvVolumeDown tvVolumeDownCommand = new TvVolumeDown(tvDevice);
 
-        // create all concrete commands for the stereo, register the receiver
+        // create all buttons for the stereo
         TurnStereoOn stereoOnCommand = new TurnStereoOn(stereoDevice);
         TurnStereoOff stereoOffCommand = new TurnStereoOff(stereoDevice);
         StereoVolumeUp stereoVolumeUpCommand = new StereoVolumeUp(stereoDevice);
